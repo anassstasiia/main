9f3895cb56d4bda35a23374c0a8087c15a55928e
Anastasia Tsirulnik <tsirulnikav92@mail.ru>
2024-11-25 22:59:38 +0700

    src

--- src/processing.py
+++ src/processing.py
@@ -1,32 +1,9 @@
-def filter_by_state(my_list, my_state):
+def filter_by_state(line: list, parameter: str = "EXECUTED") -> list:
     """Функция, которая принимает список словарей и опционально значение для ключа"""
-    return [i for i in my_list if i["state"] == my_state]
+    return [i for i in line if i["state"] == parameter]
 
 
-request = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-state = "EXECUTED"
-
-print(filter_by_state(request, state))
-
-
-def sort_by_date(my_list, my_sort=True):
+def sort_by_date(id_card: list, by_date: bool = True) -> list:
     """Функция, которая принимает список словарей и необязательный параметр,
     задающий порядок сортировки"""
-    return sorted(my_list, key=lambda x: x["date"], reverse=my_sort)
-
-
-print(
-    sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+    return sorted(id_card, key=lambda x: x["date"], reverse=by_date)
