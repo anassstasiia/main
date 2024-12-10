352b1f0b2784bedcfb6e1194b7d47dda8a6c3b6f
Anastasia Tsirulnik <tsirulnikav92@mail.ru>
2024-12-10 23:09:18 +0700

    src

--- tests/test_generator_card_number_generator.py
+++ tests/test_generator_card_number_generator.py
@@ -10,8 +10,3 @@ def test_card_number_generator_standard(card_number_generator_fixture):
 @pytest.mark.parametrize("start, finish, expected", [(2, 3, ["0000 0000 0000 0002"])])
 def test_card_number_generator(start, finish, expected):
     assert list(card_number_generator(start, finish)) == expected
-
-
-def test_card_number_generator_error():
-    with pytest.raises(ValueError):
-        card_number_generator(-5, -1)
